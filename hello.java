2a0103d95dc63f622718405f2fd1106c8ca82257
LSD_DO\kwillis <kenonwillis@gmail.com>
2018-09-21 09:44:11 -0700

    pushed it out

---
+++ hello.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class hello here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hello
+{
+   public static void main(){
+       System.out.println("will this work?");
+    }
+}
